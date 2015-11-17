0194fd3938972ba67d21e6490fc06c3e5580a992
AlexSheer <alex_sheer@inbox.lv>
2015-11-17 17:25:22 +0200

    Create README.md

---
+++ controller/postbookmark.php
@@ -0,0 +1,110 @@
+<?php
+/**
+*
+* @package phpBB Extension - Post Bookmarks
+* @copyright (c) 2015 Sheer
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace sheer\postbookmark\controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class postbookmark
+{	/** @var \phpbb	emplate	emplate */
+	protected $template;
+
+	/** @var \phpbb\db\driver\driver_interface $db */
+	protected $db;
+
+	/** @var \phpbb\user */
+	protected $user;
+
+	//** @var string phpbb_root_path */
+	protected $phpbb_root_path;
+
+	//** @var string php_ext */
+	protected $php_ext;
+
+	public function __construct(
+		\phpbb\template\template $template,
+		\phpbb\request\request_interface $request,
+		\phpbb\db\driver\driver_interface $db,
+		\phpbb\user $user,
+		$phpbb_root_path,
+		$php_ext,
+		$post_bookmark,
+		$helper
+	)
+	{
+		$this->template = $template;
+		$this->request = $request;
+		$this->db = $db;
+		$this->user = $user;
+		$this->phpbb_root_path = $phpbb_root_path;
+		$this->php_ext = $php_ext;
+		$this->postbookmark_table = $post_bookmark;
+		$this->helper = $helper;
+	}
+
+	public function main()
+	{		$topic_id = $this->request->variable('t', 0);
+		$post_id = $this->request->variable('p', 0);
+		$forum_id = $this->request->variable('f', 0);
+		$mode = $this->request->variable('mode', '');
+		$post_form = $this->request->variable('form', '');
+
+		$book_submit = request_var('book', false);
+		$book_cancel = request_var('reset', false);
+
+		$viewtopic_url = append_sid("{$this->phpbb_root_path}viewtopic." . $this->php_ext . "", "f=$forum_id&amp;t=$topic_id");
+		$body = 'add_bookmark';
+
+		if($mode == 'delete')
+		{			$sql = 'DELETE FROM ' . $this->postbookmark_table . "
+				WHERE user_id = {$this->user->data['user_id']}
+					AND post_id = $post_id";
+			$this->db->sql_query($sql);
+			$message = $this->user->lang['POST_BOOKMARK_REMOVED'] . '<br /><br />' . sprintf($this->user->lang['RETURN_TOPIC'], '<a href="' . $viewtopic_url . '">', '</a>');
+			meta_refresh(3, $viewtopic_url);
+			trigger_error($message);
+		}
+		else if($mode == 'find')
+		{			$body = 'find_bookmark';
+			$this->helper->get_bookmarks();
+		}
+		else
+		{			$bookmark_desc = $this->request->variable('bookmark_desc', '', true);
+			if($book_cancel)
+			{
+				redirect($viewtopic_url);
+			}
+			if ($book_submit)
+			{				$sql = 'INSERT INTO ' . $this->postbookmark_table . ' ' . $this->db->sql_build_array('INSERT', array(
+					'user_id'		=> $this->user->data['user_id'],
+					'post_id'		=> $post_id,
+					'topic_id'		=> $topic_id,
+					'bookmark_time'	=> time(),
+					'bookmark_desc'	=> $bookmark_desc,
+				));
+				$this->db->sql_query($sql);				$message = $this->user->lang['POST_BOOKMARK_ADDED'] . '<br /><br />' . sprintf($this->user->lang['RETURN_TOPIC'], '<a href="' . $viewtopic_url . '">', '</a>');
+				meta_refresh(3, $viewtopic_url);
+				trigger_error($message);
+			}
+		}
+
+		$this->template->assign_vars(array(
+			'U_POST_ACTION'			=> append_sid("{$this->phpbb_root_path}postbookmark", "f=$forum_id&amp;t=$topic_id&amp;p=$post_id&amp;mode=$mode"),
+			'S_POST_FORM'			=> $post_form,
+			)
+		);
+
+		page_header($this->user->lang['POST_BOOKMARK_ADD']);
+		$this->template->set_filenames(array(
+			'body' => $body . '.html'));
+
+		page_footer();
+		return new Response('', 200);
+	}
+}
---
+++ core/helper.php
@@ -0,0 +1,125 @@
+<?php
+/**
+*
+* @package phpBB Extension - Post Bookmarks
+* @copyright (c) 2015 Sheer
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace sheer\postbookmark\core;
+
+class helper
+{
+	/** @var \phpbb\config\config */
+	protected $config;
+
+	/** @var \phpbb\db\driver\driver_interface */
+	protected $db;
+
+	/** @var \phpbb\auth\auth */
+	protected $auth;
+
+	/** @var \phpbb\template\template */
+	protected $template;
+
+	/** @var \phpbb\user */
+	protected $user;
+
+	/** @var \phpbb\cache\driver\driver_interface */
+	protected $cache;
+
+	/** @var \phpbb\request\request_interface */
+	protected $request;
+
+	/** @var \phpbb\notification\manager */
+	protected $notification_manager;
+
+	/** @var string phpbb_root_path */
+	protected $phpbb_root_path;
+
+	/** @var string phpEx */
+	protected $php_ext;
+
+	/** @var string table_prefix */
+	protected $table_prefix;
+
+	/** @var \phpbb\pagination */
+	protected $pagination;
+
+	/**
+	* Constructor
+	*/
+	public function __construct(
+		\phpbb\config\config $config,
+		\phpbb\db\driver\driver_interface $db,
+		\phpbb\template\template $template,
+		\phpbb\user $user,
+		\phpbb\request\request_interface $request,
+		\phpbb\pagination $pagination,
+		$phpbb_root_path,
+		$php_ext,
+		$table_prefix
+	)
+	{
+		$this->config = $config;
+		$this->db = $db;
+		$this->template = $template;
+		$this->user = $user;
+		$this->request = $request;
+		$this->pagination = $pagination;
+		$this->phpbb_root_path = $phpbb_root_path;
+		$this->php_ext = $php_ext;
+		$this->table_prefix = $table_prefix;
+	}
+
+	public function get_bookmarks()
+	{
+		define ('POSTS_BOOKMARKS_TABLE', $this->table_prefix.'posts_bookmarks');
+
+		$start = $this->request->variable('start', 0);
+
+		$sql =  'SELECT COUNT(post_id) as posts_count
+			FROM ' . POSTS_BOOKMARKS_TABLE . '
+			WHERE user_id = ' . $this->user->data['user_id'];
+
+		$result = $this->db->sql_query($sql);
+		$posts_count = (int) $this->db->sql_fetchfield('posts_count');
+		$this->db->sql_freeresult($result);
+
+		$pagination_url = append_sid("{$this->phpbb_root_path}postbookmark", "mode=find");
+		$this->pagination->generate_template_pagination($pagination_url, 'pagination', 'start', $posts_count, $this->config['topics_per_page'], $start);
+
+		$sql = 'SELECT b.post_id AS b_post_id, b.user_id, b.bookmark_time, b.bookmark_desc, p.post_id, p.forum_id, p.topic_id, p.poster_id, p.post_subject, p.post_time, u.user_id, u.username, u.user_colour
+			FROM ' . POSTS_BOOKMARKS_TABLE . ' b
+			LEFT JOIN ' . POSTS_TABLE . ' p ON( b.post_id = p.post_id)
+			LEFT JOIN ' . USERS_TABLE . ' u ON (p.poster_id = u.user_id)
+			WHERE b.user_id = ' . $this->user->data['user_id'] . '
+			ORDER BY b.bookmark_time ASC';
+		$result = $this->db->sql_query_limit($sql, $this->config['topics_per_page'], $start);
+
+		while ($row = $this->db->sql_fetchrow($result))
+		{
+			// Send vars to template
+			$this->template->assign_block_vars('postrow', array(
+				'POST_ID'			=> $row['b_post_id'],
+				'POST_TIME'			=> $this->user->format_date($row['post_time']),
+				'BOOKMARK_TIME'		=> $this->user->format_date($row['bookmark_time']),
+				'BOOKMARK_DESC'		=> $row['bookmark_desc'],
+				'LAST_POST_SUBJECT'	=> $row['post_subject'],
+				'TOPIC_AUTHOR'		=> get_username_string('full', $row['user_id'], $row['username'], $row['user_colour']),
+				'POST_TITLE'		=> $row['post_subject'],
+				'U_VIEW_POST'		=> append_sid("{$this->phpbb_root_path}viewtopic.$this->php_ext", "p=" . $row['post_id'] . "#p" . $row['post_id'] . ""),
+				'S_DELETED_TOPIC'	=> (!$row['topic_id']) ? true : false,
+				'S_DELETED_POST'	=> (!$row['post_id']) ? true : false,
+				'U_POST_BOOKMARK'	=> '[url='. generate_board_url() .'/viewtopic.'. $this->php_ext .'?p=' . $row['post_id'] . '#p' . $row['post_id'] . ']' . $row['post_subject'] . '[/url]',
+			));
+		}
+		$this->db->sql_freeresult($result);
+
+		$this->template->assign_vars(array(
+			'TOTAL_BOOKMARKS'	=> $this->user->lang('TOTAL_BOOKMARKS', (int) $posts_count),
+			'PAGE_NUMBER'		=> $this->pagination->on_page($posts_count, $this->config['topics_per_page'], $start),
+		));
+	}
+}
